197717cfe9c4df7833479e43973ccab7364b461b
Panos Kaf <panoskatsalifis@gmail.com>
2024-07-08 00:24:25 +0300

    Java Worldtree Pre-Order insertion

--- java/Worldtree/worldtree.java
+++ java/Worldtree/worldtree.java
@@ -1,59 +1,85 @@
 import java.util.*;
 
-
 class Node{
     int value;
     Node left,right;
     Node(int val){
         this.value = val;
-        this.left=this.right=null;
+        this.left=null;
+        this.right=null;
     }
 }
 
 public class Worldtree {
     
     Node root;
+    int size;
 
-    public Worldtree(){
+    public boolean isFull(Node n){
+        if(n.left!=null && n.right!=null)
+        return n.left.value==0 && n.right.value==0 || (isFull(n.left) && isFull(n.right));
+        else return false;
+    }
+
+    public Worldtree(int n){
+        this.size=n;
         this.root = null;
     }
 
-    public void insert(Node r,int val){
-        if(r==null){ 
-            r = new Node(val);
+    public void insert(int val){
+        root = insertHelper(root, val);
+    }
+
+    public Node insertHelper(Node n, int val){
+        if(n==null){
+            n = new Node(val);
+            return n;
         }
-        else if (r.left==null) r.left = new Node(val);
-        else if (r.left.value!=0) insert(r.left,val);
-        else if (r.right==null) r.right = new Node(val);
-        else if (r.right.value!=0) insert(r.right,val);
-        else {
+        if(n.value!=0){
+            if (n.left == null){
+                n.left = new Node(val);
+                return n;
+            }
+            else if (n.left.value != 0 && !isFull(n.left)){
+                n.left = insertHelper(n.left,val);
+            }
+            else if (n.right == null){
+                n.right = new Node(val);
+                return n;
+            }   
+            else if (n.right.value != 0 && !isFull(n.right)){
+                n.right = insertHelper(n.right,val);
+            }
+
+        else{
             System.out.println("The Worldtree is full");
         }
     }
+        return n; 
+    }
 
-    public void inorder(Node r){
+    public void inorder(){
+        inorderHelper(this.root);
+    }
+    public void inorderHelper(Node r){
     if(r!=null){
-        inorder(r.left);
+        inorderHelper(r.left);
             if(r.value!=0){
                 System.out.print(r.value + " ");
             }
-        inorder(r.right);
+        inorderHelper(r.right);
         }
     }   
-
     public static void main(String[] args) {
 
-        Worldtree worldtree = new Worldtree();
-        wordltree.insert(worldtree.root,1);
-        wordltree.insert(worldtree.root,3);
-        wordltree.insert(worldtree.root,4);
-        wordltree.insert(worldtree.root,5);
-        wordltree.insert(worldtree.root,6);
-        wordltree.insert(worldtree.root,0);
-        wordltree.insert(worldtree.root,0);
-        wordltree.insert(worldtree.root,0);
-        wordltree.insert(worldtree.root,0);
-        worldtree.inorder(worldtree.root);
+        Scanner userInput = new Scanner(System.in);
+        int N = userInput.nextInt();
+        Worldtree worldtree = new Worldtree(N);
+        for(int i=0;i<2*N+1;i++){
+            worldtree.insert(userInput.nextInt());
+        }
+        userInput.close();
+        worldtree.inorder();
     }
 
 }
