950a9c566d52c9e27cdc0473a1691f83af863673
Panos Kaf <panoskatsalifis@gmail.com>
2024-07-12 15:25:15 +0300

    Cleaned up ML dir and added C dir

--- C/fairSeqeunce/fairseq.c
+++ C/fairSeqeunce/fairseq.c
@@ -1,54 +1,45 @@
 #include <stdio.h>
 
 int abs(int x){
-	if(x<0) return -x;
-	else return x;
+    if(x<0) return -x;
+    else return x;
 }
 
-int findSum(int N,int* array){
-	int sum=0;
-	for(int i=0;i<N;i++)
-		sum+=*(array+i);
-	return sum;
-}
-
-void findBest(int* best,int val,int total){
-	int current=abs(2*val-total);
-	if(*best>current) *best=current; 
+int findBest(int* best,int partial,int total){
+    int current=abs((2*partial)-total);
+    if(*best>current) {
+        *best=current; 
+        return 1;
+    }
+    return 0;
 }
 
 int main(int argc, char** argv){
-	
-	int N=5;
-	//FILE* file=fopen(argv[1],"r");
-	//scanf("%d",&N);
-	int sequence[5]={1,2,3,4,5};
+    
+    int N=1;
+    FILE* file=fopen(argv[1],"r");
+    fscanf(file,"%d",&N);
+    int sequence[N],totalSum=0;
+    for(int i=0;i<N;i++) {
+        fscanf(file,"%d",&sequence[i]);
+        totalSum+=sequence[i];
+    }
 
-	//for(int i=0;i<N;i++) scanf("%d",&sequence[i]);
+    int best=totalSum;
+    int j=0,new_j=0,partialSum=0;
 
-	const int totalSum=findSum(N,sequence);
-	int best=totalSum;
-
-	int new_j=0,tmpSum=0;
-
-	for(int i=0;i<N;i++){
-
-		if(best==0 ||(best==1 && totalSum%2==1)) break;
-
-		for(int j=new_j;j<N;j++){
-		
-			if(new_j!=N-1)tmpSum+=sequence[j];
-			if(((2*tmpSum-totalSum)>=0)){
-				findBest(&best,tmpSum,totalSum);
-				if(j!=new_j)findBest(&best,tmpSum-sequence[j],totalSum);
-				new_j=j;
-				break;
-			}
-		}
-		tmpSum-=sequence[i];
-	}
-
-	printf("%d\n",best);
-
-	return 0;	
+    for(int i=0;i<N;i++){
+        if(best<2) break;
+        for(j=new_j;j<N;j++){
+            partialSum+=sequence[j];
+            if((2*partialSum-totalSum)>0){
+                if(findBest(&best,partialSum,totalSum)) new_j=j+1;
+                if(findBest(&best,partialSum-sequence[j],totalSum)){new_j=j; partialSum-=sequence[j];}
+                break;
+            }
+        }
+        partialSum-=sequence[i];
+    }
+    printf("%d\n",best);
+    return 0;	
 }
