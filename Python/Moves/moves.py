b84338fa707c9ee3266f8efcc160b8a1572f08ee
Panos Kaf <panoskatsalifis@gmail.com>
2024-07-13 21:52:22 +0300

    Python Moves 1.1 - wrapped solver in class - grader version

--- Python/Moves/moves.py
+++ Python/Moves/moves.py
@@ -1,86 +1,84 @@
 import sys
 from collections import deque
 
-def solve(n,grid):
-    frontier = deque()
-    loc = (0,0)
-    parent = {(0,0):None}
-    distance = {(0,0):0}
-    directions = {}
-    frontier.append(loc)
+class Moves:
 
-    while(frontier):
-        loc = frontier.pop()
-        x,y = loc
-        currentDistance = distance.get(loc)
-        if loc==(n-1,n-1):
-            return reconstruct(parent,directions,(n-1,n-1))
-        neighbors = Neighbors(n,grid,x,y)
+    def __init__(self,n,grid):
+        self.n = n
+        self.grid = grid    
+
+    def reconstruct(self,parent,dir,end):
+        path = []
+        loc = end
+        while not loc==None:
+            if dir.__contains__(loc):
+                path.append(dir[loc])
+            loc = parent[loc]
+        return path
+
+    def Neighbors(self,x,y):
+        res = {}
+
+        neighbors = [
+            (-1,-1),
+            (-1, 0),
+            (-1,+1),
+            (0, -1),
+            (0, +1),
+            (+1,-1),
+            (+1, 0),
+            (+1,+1)
+        ]
+
+        directions = {
+            neighbors[0]:"NW",
+            neighbors[1]:"N",
+            neighbors[2]:"NE",
+            neighbors[3]:"W",
+            neighbors[4]:"E",
+            neighbors[5]:"SW",
+            neighbors[6]:"S",
+            neighbors[7]:"SE",
+            }
+        
         for neighbor in neighbors:
-            dir = neighbors.get(neighbor)
-            if not distance.__contains__(neighbor):
-                frontier.append(neighbor)
-                distance[neighbor]=currentDistance+1
-                parent[neighbor]=loc
-                directions[neighbor]=dir
-    return None
+            xn = x + neighbor[0]
+            yn = y + neighbor[1]
+            if xn >= 0 and yn >= 0 and xn < self.n and yn < self.n and self.grid[xn][yn]<self.grid[x][y]:
+                res[(xn,yn)] = directions[neighbor]
+        return res
 
-def reconstruct(parent,dir,end):
-    path = []
-    loc = end
-    while not loc==None:
-        if dir.__contains__(loc):
-            path.append(dir[loc])
-        loc = parent[loc]
-    return path
+    def solve(self):
+        frontier = deque()
+        loc = (0,0)
+        end = (self.n-1, self.n-1)
+        parent = {(0,0):None}
+        distance = {(0,0):0}
+        directions = {}
+        frontier.append(loc)
 
-def Neighbors(n,grid,x,y):
-    res = {}
-
-    neighbors = [
-        (-1,-1),
-        (-1, 0),
-        (-1,+1),
-        (0, -1),
-        (0, +1),
-        (+1,-1),
-        (+1, 0),
-        (+1,+1)
-    ]
-
-    directions = {
-        neighbors[0]:"NW",
-        neighbors[1]:"N",
-        neighbors[2]:"NE",
-        neighbors[3]:"W",
-        neighbors[4]:"E",
-        neighbors[5]:"SW",
-        neighbors[6]:"S",
-        neighbors[7]:"SE",
-        }
-    
-    for neighbor in neighbors:
-        xn = x + neighbor[0]
-        yn = y + neighbor[1]
-        if xn >= 0 and yn >= 0 and xn < n and yn < n and grid[xn][yn]<grid[x][y]:
-            res[(xn,yn)] = directions[neighbor]
-    return res
-
+        while(frontier):
+            loc = frontier.pop()
+            x,y = loc
+            currentDistance = distance.get(loc)
+            if loc==(n-1,n-1):
+                return self.reconstruct(parent,directions,(n-1,n-1))
+            neighbors = self.Neighbors(x,y)
+            for neighbor in neighbors:
+                dir = neighbors.get(neighbor)
+                if not distance.__contains__(neighbor):
+                    frontier.append(neighbor)
+                    distance[neighbor]=currentDistance+1
+                    parent[neighbor]=loc
+                    directions[neighbor]=dir
+        return None
 
 with open(sys.argv[1], 'r') as file:
     n = int(file.readline())
     grid = [list(map(int, line.split())) for line in file]
 
-print('n =',n,'\n\nGrid:')
-for row in grid:
-    print('~  ',end='')
-    for x in row:
-        print(x,' ',end='')
-    print()
-
-print()
-
-path = solve(n,grid)
+solver = Moves(n,grid)
+path = solver.solve()
 
 if path == None:
     print("IMPOSSIBLE")
@@ -89,7 +87,3 @@ else:
     for direction in path[:-1]:
         print(direction,', ',sep='',end='')
     print(path[-1],']',sep='')
-
-
-
-
