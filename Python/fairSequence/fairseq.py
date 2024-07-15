f21ef3e3584d95362d93bea5ba9983bc67850d7b
Panos Kaf <panoskatsalifis@gmail.com>
2024-07-15 20:37:39 +0300

    Python fairseq

--- Python/fairSequence/fairseq.py
+++ Python/fairSequence/fairseq.py
@@ -1,40 +1,21 @@
 import sys
 
-def findBest(best, p, t):
-    current = abs(2*p - t)
-    if best > current:
-        best = current
-    return best
+def findBest(i, partial):
+    if i == n: 
+        return abs(2*partial - total)
+    
+    if (i, partial) in bestFound: 
+        return bestFound[(i, partial)]
 
-n = 0
+    bestFound[(i,partial)] = min(findBest(i+1, partial),findBest(i+1, partial+seq[i]))
+    return bestFound[(i,partial)]
+
 seq = []
-
 with open(sys.argv[1]) as f:
     n = int(f.readline())
     for num in f.readline().split():
         seq.append(int(num))
 
 total = sum(seq)
-best = total
-j=0
-new_j = 0
-partial = 0
-
-for i in range(n):
-    if best<2:
-        break
-    for j in range(new_j,n):
-        partial += seq[j] 
-        if 2*partial > total : 
-            temp = findBest(best,partial,total)
-            if temp<best :
-                best = temp
-                new_j = j+1
-            temp = findBest(best,partial-seq[j],total)
-            if temp<best :
-                new_j = j
-                partial -= seq[j]
-            break
-    partial -= seq[i]
-
-print(best)
+bestFound = {}
+print(findBest(0,0))
